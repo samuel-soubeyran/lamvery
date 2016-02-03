ea004f7ef00be3c0444c6e6f78f7121dd3e8c67c
Masashi Terui <masashi.terui@jig-saw.com>
2016-02-03 19:47:06 +0900

    [WIP] Add logs command

---
+++ lamvery/actions/logs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from lamvery.actions.base import BaseAction
+
+
+class LogsAction(BaseAction):
+
+    def __init__(self, args):
+        super(LogsAction, self).__init__(args)
+
+    def action(self):
+        pass
